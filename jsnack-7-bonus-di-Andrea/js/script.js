43a311db88e43f98b1ae416dcdd9f31695859114
Marco <marco.longo@hotmail.it>
2020-06-19 17:50:04 +0200

    Esercizio Bonus Andrea (completo).

--- jsnack-7-bonus-di-Andrea/js/script.js
+++ jsnack-7-bonus-di-Andrea/js/script.js
@@ -2,6 +2,13 @@
 
 // Dichiarazione variabili
 var iniziaCalcolo = document.getElementById('iniziaCalcolo');
+// Dichiarazione funzione
+function compare(a, b) {
+  if (a > b) return 1;
+  if (b > a) return -1;
+
+  return 0;
+}
 
 // Listener sul tasto Inizia Calcolo
 iniziaCalcolo.addEventListener("click", function () {
@@ -17,15 +24,48 @@ iniziaCalcolo.addEventListener("click", function () {
     document.getElementById('authTravel').innerHTML = "Sì, numero consentito."
     // Finché il contatore è minore del numero partecipanti, mostro prompt, immagazzino età nel posto count dell'array e incremento count
     do {
-    eta[count] = parseInt(prompt("Inserisci età dell'invitato numero " + count));
+    eta[count] = parseInt(prompt("Inserisci età dell'invitato numero " + count + ":"));
     console.log(eta[count]);
     count++;
     }
     while (count < numeroPersone);
 
+    // Inietta i valori dell'array eta, uno per uno, in una stringa e separali da spazio
+    var printThis = "";
+
+    for(var i = 0; i < eta.length; i++){
+        printThis += " "+eta[i];
+    }
+    // Stampa i valori di età nell'ordine di inserimento dell'utente
+    document.getElementById('listaEta').innerHTML = printThis;
+
     // Mostro età del partecipante più vecchio e di quello più giovane mostrandolo in 2 span
     document.getElementById('oldestOne').innerHTML = Math.max.apply(Math, eta);
     document.getElementById('youngestOne').innerHTML = Math.min.apply(Math, eta);
+
+    var dateBirth = "";
+
+    for(var i = 0; i < eta.length; i++){
+        eta[i] = 2020 - eta[i];
+        dateBirth += " "+eta[i];
+    }
+
+    document.getElementById('listaNascit').innerHTML = dateBirth;
+
+    // Ordina crescentemente i valori dell'array
+    eta = eta.sort(compare);
+
+    // Inietta i valori dell'array eta, uno per uno, in una stringa e separali da spazio
+    var printThisOrd = "";
+    var i = 0;
+
+    for(var i = 0; i < eta.length; i++){
+        eta[i] = 2020 - eta[i];
+        printThisOrd += " "+eta[i];
+    }
+    // Stampa i valori di età nell'ordine crescente
+    document.getElementById('listaEtaCresc').innerHTML = printThisOrd;
+
   } else {
     // Mostro diniego a procedere per eccessivi partecipanti
     document.getElementById('authTravel').innerHTML = "No, massimo 10 persone, minimo una!"
