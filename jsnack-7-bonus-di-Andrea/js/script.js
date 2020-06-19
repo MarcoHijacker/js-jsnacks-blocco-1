1efba9d0e0af0e5e64a180a503169fd60bab65a6
Marco <marco.longo@hotmail.it>
2020-06-19 16:44:08 +0200

    Esercizio Bonus Andrea (parziale)

--- jsnack-7-bonus-di-Andrea/js/script.js
+++ jsnack-7-bonus-di-Andrea/js/script.js
@@ -3,14 +3,19 @@
 // Dichiarazione variabili
 var iniziaCalcolo = document.getElementById('iniziaCalcolo');
 
+// Listener sul tasto Inizia Calcolo
 iniziaCalcolo.addEventListener("click", function () {
   var numeroPersone = parseInt(document.getElementById('numeroPersone').value);
+  // Contatore per ciclo do-while
   var count = 0;
+  // Nuovo array contenente tutte le età dei partecipanti
   eta = new Array();
 
+  // Verifica di sicurezza per massimale partecipanti
   if (numeroPersone <= 10 && numeroPersone > 0 && numeroPersone != 0) {
-
+    // Stampo autorizzazione o meno a procedere
     document.getElementById('authTravel').innerHTML = "Sì, numero consentito."
+    // Finché il contatore è minore del numero partecipanti, mostro prompt, immagazzino età nel posto count dell'array e incremento count
     do {
     eta[count] = parseInt(prompt("Inserisci età dell'invitato numero " + count));
     console.log(eta[count]);
@@ -18,9 +23,11 @@ iniziaCalcolo.addEventListener("click", function () {
     }
     while (count < numeroPersone);
 
+    // Mostro età del partecipante più vecchio e di quello più giovane mostrandolo in 2 span
     document.getElementById('oldestOne').innerHTML = Math.max.apply(Math, eta);
     document.getElementById('youngestOne').innerHTML = Math.min.apply(Math, eta);
   } else {
+    // Mostro diniego a procedere per eccessivi partecipanti
     document.getElementById('authTravel').innerHTML = "No, massimo 10 persone, minimo una!"
   }
 
